e6c85eae28b95bc5a88701ce3b5a2e1c272c9477
UCLAB416 <UCLAB416@UCLAB416>
2024-08-12 19:22:22 -0500

    Agregar archivos de proyecto.

---
+++ Class/ListaSimple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class ListaSimple
+    {
+        nodo inicio = null;
+        //insertar
+        
+        //mostrar
+    }
+}
---
+++ Class/nodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class nodo /*Agregamos el nodo*/
+    {
+        /* 0 w 0 */
+        public int dato; /* Agregamos el dato */ 
+        public nodo sig = null; /* Agregamso mel puntero de momento apunta al null */
+    }
+}
